21cc436632dc9c32ebae1dbff67bb92752fb4205
CUnknown <qwertynotasdfgh@protonmail.com>
2024-08-13 14:27:39 +0100

    add choice possibilities

--- rps.py
+++ rps.py
@@ -2,7 +2,31 @@ import random
 
 # Game code:
 def game():
-    pass
+    # Computers choice:
+    item_choices = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(item_choices)
+    # Scores
+    comp_score = 0
+    player_score = 0
+    # Game logic:
+    while comp_score < 3 or player_score < 3:
+        player_choice = input("Choose 'ROCK', 'PAPER' OR 'SCISSORS'")
+        if player_choice == computer_choice:
+            print("Draw! \nRun it back!")
+        elif player_choice.lower() == 'rock' and computer_choice == 'paper':
+            pass
+        elif player_choice.lower() == 'rock' and computer_choice == 'scissors':
+            pass
+        elif player_choice.lower() == 'paper' and computer_choice == 'rock':
+            pass
+        elif player_choice.lower() == 'paper' and computer_choice == 'scissors':
+            pass
+        elif player_choice.lower() == 'scissors' and computer_choice == 'paper':
+            pass
+        elif player_choice.lower() == 'scissors' and computer_choice == 'rock':
+            pass
+    
+
 
 # Ask player wether or not the want to play the game:
 def do_you_want_to_play():
